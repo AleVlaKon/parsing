0c2aa4f15ea5277c10f770899b8d5a8d99ac6361
AleVlaKon <kontikov@mail.ru>
2024-05-06 22:48:33 +0300

    Сделана задача 4.9.6

--- 4.9.6.py
+++ 4.9.6.py
@@ -10,13 +10,14 @@ with open('res.csv', 'w', newline='', encoding='utf-8-sig') as file:
             html = requests.get(f'https://parsinger.ru/html/index{cat}_page_{pag}.html')
             html.encoding = 'utf-8'
             soup = BeautifulSoup(html.text, 'lxml')
-            for link in soup.select('div.item_card div.item'):
-                print(len(link))
-                # name = soup.select_one('a.name_item').text.strip()
-                # attributes = [i.text.split(':')[1].strip() for i in soup.select('.description li')]
-                # price = soup.select_one('p.price').text.strip()
-                # print(attributes)
-                # writer.writerow([name] + attributes + [price])
-        break
+            for link in soup.find_all('div', class_='img_box'):
+                # print(len(link))
+                
+                name = link.select_one('a.name_item').text.strip()
+                attributes = [i.text.split(':')[1].strip() for i in link.select('.description li')]
+                price = link.select_one('p.price').text.strip()
+                print([name] + attributes + [price])
+                writer.writerow([name] + attributes + [price])
+        
 
 
