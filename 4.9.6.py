fc77e4249b685bb59587a4a66c4e9d6d37f236fd
AleVlaKon <kontikov@mail.ru>
2024-05-06 22:56:14 +0300

    Допилил задачу 4.9.6 (убрал лишние строки)

--- 4.9.6.py
+++ 4.9.6.py
@@ -11,8 +11,6 @@ with open('res.csv', 'w', newline='', encoding='utf-8-sig') as file:
             html.encoding = 'utf-8'
             soup = BeautifulSoup(html.text, 'lxml')
             for link in soup.find_all('div', class_='img_box'):
-                # print(len(link))
-                
                 name = link.select_one('a.name_item').text.strip()
                 attributes = [i.text.split(':')[1].strip() for i in link.select('.description li')]
                 price = link.select_one('p.price').text.strip()
