3e5443eb67e7447287962094ae37e8642e2b05bb
Алексей Контиков <av.kontikov@isppp.ru>
2024-05-06 16:58:39 +0300

    Неправильно сделана задача 4.9.6 еще раз

--- 4.9.6.py
+++ 4.9.6.py
@@ -2,25 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-list_of_links = []
 
-begin = 'https://parsinger.ru/html/'
-for cat in range(1, 6):
-    for pag in range(1, 5):
-        html = requests.get(f'https://parsinger.ru/html/index{cat}_page_{pag}.html')
-        html.encoding = 'utf-8'
-        soup = BeautifulSoup(html.text, 'html.parser')
-        list_pagen = [f'{begin}{i.get("href")}' for i in soup.select('a.name_item')]
-        list_of_links.extend(list_pagen)
+with open('res.csv', 'w', newline='', encoding='utf-8-sig') as file:
+    writer = csv.writer(file, delimiter=';')
+    for cat in range(1, 6):
+        for pag in range(1, 5):
+            html = requests.get(f'https://parsinger.ru/html/index{cat}_page_{pag}.html')
+            html.encoding = 'utf-8'
+            soup = BeautifulSoup(html.text, 'lxml')
+            for link in soup.select('div.item_card div.item'):
+                print(len(link))
+                # name = soup.select_one('a.name_item').text.strip()
+                # attributes = [i.text.split(':')[1].strip() for i in soup.select('.description li')]
+                # price = soup.select_one('p.price').text.strip()
+                # print(attributes)
+                # writer.writerow([name] + attributes + [price])
+        break
 
-for i in list_of_links:
-    print(i)
 
-# with open('res.csv', 'w', newline='', encoding='utf-8-sig') as file:
-#     writer = csv.writer(file, delimiter=';')
-
-#     for link in list_of_links:
-#         name = soup.select_one('a.name_item').text.strip()
-#         attributes = [i.text.split(':')[1].strip() for i in soup.select('#description li')]
-#         price = soup.select_one('p.price').text.strip()
-#         writer.writerow([name] + attributes + [price])
