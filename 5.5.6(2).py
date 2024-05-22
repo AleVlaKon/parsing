a87126c050588eedd23285cb6bbeede1d3f93bbb
AleVlaKon <kontikov@mail.ru>
2024-05-22 22:39:51 +0300

    Повторно сделана задача 5.5.6

--- 5.5.6(2).py
+++ 5.5.6(2).py
@@ -4,13 +4,13 @@ from selenium.webdriver.common.by import By
 
 
 with webdriver.Chrome() as browser:
-    browser.get('https://parsinger.ru/selenium/5.5/5/test/test.html')
+    browser.get('https://parsinger.ru/selenium/5.5/5/1.html')
     for element in browser.find_elements(By.XPATH, '/html/body/div/div'):
         color_hex = element.find_element(By.TAG_NAME, 'span').text
         element.find_element(By.CSS_SELECTOR, f'option[value="{color_hex}"]').click()
         element.find_element(By.CSS_SELECTOR, f'button[data-hex="{color_hex}"]').click()
         element.find_element(By.CSS_SELECTOR, 'input[type="checkbox"]').click()
         element.find_element(By.CSS_SELECTOR, f'input[type="text"]').send_keys(color_hex)
-        element.find_element(By.CSS_SELECTOR, 'button').click()
-    element.find_element(By.CSS_SELECTOR, 'button').click()
+        element.find_element(By.XPATH, ".//button[text()='Проверить']").click()
+    browser.find_element(By.XPATH, ".//button[text()='Проверить все элементы']").click()
     time.sleep(15)
