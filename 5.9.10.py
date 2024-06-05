92b5b83f330fa65c941443e7117ea565453065c7
AleVlaKon <kontikov@mail.ru>
2024-06-05 23:11:10 +0300

    Проверил, что возвращает объект WebDriverWait и начал делать задачу 5.9.13

--- 5.9.10.py
+++ 5.9.10.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 ids_to_find = ['xhkVEkgm', 'QCg2vOX7', '8KvuO5ja', 
                'CFoCZ3Ze', '8CiPCnNB', 'XuEMunrz', 
@@ -13,9 +14,14 @@ ids_to_find = ['xhkVEkgm', 'QCg2vOX7', '8KvuO5ja',
 with webdriver.Chrome() as browser:
     browser.get('https://parsinger.ru/selenium/5.9/3/index.html')
     elements = [browser.find_element(By.ID, ids) for ids in ids_to_find]
+    time.sleep(1)
+    zero_el = browser.find_element(By.CLASS_NAME, 'box')
+    print(zero_el.__class__)
 
     for element in elements:
-        element = WebDriverWait(browser, 60).until(EC.visibility_of(element)).click()
+        element = WebDriverWait(browser, 60).until(EC.visibility_of(element))
+        print(element.__class__)
+        element.click()
         print('!')
     
     print(browser.switch_to.alert.text)
---
+++ 5.9.13.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+with webdriver.Chrome() as browser:
+    browser.get('https://parsinger.ru/selenium/5.9/4/index.html')
+    
