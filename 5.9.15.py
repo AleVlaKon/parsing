18ac192ac5725fa72d3605dedccf170b1e791968
Алексей Контиков <av.kontikov@isppp.ru>
2024-06-07 13:56:57 +0300

    Сделаны задачи 5.9.15-16

---
+++ 5.9.15.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+with webdriver.Chrome() as browser:
+    browser.get('https://parsinger.ru/selenium/5.9/6/index.html')
+    WebDriverWait(browser, 10).until(EC.element_located_to_be_selected((By.ID, "myCheckbox")))
+    browser.find_element(By.TAG_NAME, 'button').click()
+    print(browser.find_element(By.ID, "result").text)
+    time.sleep(10)
+
+        
+        
