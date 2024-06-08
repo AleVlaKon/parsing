fe01b8dec6484314c9c2774dc5cce19303c4c73e
AleVlaKon <kontikov@mail.ru>
2024-06-08 22:50:08 +0300

    Сделана задача 5.6 в курсе по selenium

--- test2.py
+++ test2.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
 
 with webdriver.Chrome() as browser:
-    browser.get('https://parsinger.ru/selenium/5.9/4/index.html')
+    browser.get('https://vk.com')
+    print(browser.title)
+    browser.get('https://ya.ru')
+    browser.back()
+    assert browser.title == 'ВКонтакте | Добро пожаловать'
+
+    
+
+
     
