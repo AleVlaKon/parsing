10000db8fc3c019529cddc9e13d16926e656a428
AleVlaKon <kontikov@mail.ru>
2024-06-08 22:41:00 +0300

    Изменен файл test2 (тест hh)

--- test2.py
+++ test2.py
@@ -1,18 +1,9 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-with webdriver.Chrome() as driver:
-    driver.get('https://parsinger.ru/selenium/5.8/4/index.html')
 
-    # Переключаемся на iframe
-    iframe_element = driver.find_element(By.TAG_NAME, 'iframe')
-    driver.switch_to.frame(iframe_element)
-
-    # Извлекаем HTML содержимое из iframe
-    iframe_content = driver.page_source
-
-    print(iframe_content)
-    time.sleep(14)
-
+with webdriver.Chrome() as browser:
+    browser.get('https://parsinger.ru/selenium/5.9/4/index.html')
+    
